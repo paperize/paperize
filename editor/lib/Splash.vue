0e88a07112582f25a97c457ec0b7e46a17fe611d
Loren Norman <lorennorman@gmail.com>
2017-07-17 20:48:57 -0400

    start a splash page

---
+++ editor/lib/Splash.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div
+  title-bar
+</template>
+
+<script>
+
+  import TitleBar from './TitleBar.vue'
+
+  export default {
+    components: {
+      "title-bar": TitleBar
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
--- editor/lib/routes.js
+++ editor/lib/routes.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import Home from './Home.vue'
+import Splash from './Splash.vue'
 import GameEditor from './GameEditor.vue'
 
 const routes = [
-  { path: '/',          component: Home, props: true },
+  { path: '/',              component: Splash,     props: true },
+  // { path: '/games',         component: GameIndex,  props: true },
   { path: '/games/:gameId', component: GameEditor, props: true }
 ]
 
