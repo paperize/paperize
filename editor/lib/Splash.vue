d85d56bed5cda105f829432a8eb102bc68bc9e86
Loren Norman <lorennorman@gmail.com>
2017-07-19 11:13:53 -0400

    placeholder splash message

--- editor/lib/Splash.vue
+++ editor/lib/Splash.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
 div
   title-bar
+
+  .row
+    .small-12.columns
+      h2 Welcome to New Paperize!
+
+      p We're in beta right now, just starting to put the basic pieces together, but we can still have some fun.
+      p
+        em Sign in above and we'll get started playing with games and their components.
 </template>
 
 <script>
