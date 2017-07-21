ab0ce3b173cb396f20d2a7a2333232d7ba653f12
Loren Norman <lorennorman@gmail.com>
2017-07-21 17:51:04 -0400

    bring in a Vuex store

--- editor/lib/main.js
+++ editor/lib/main.js
@@ -1,13 +1,17 @@
 console.log("Loading Paperize Editor...")
 
 import Vue from 'vue'
-
+import Vuex from 'vuex'
 import { router } from './routes'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({})
 
 let startApp = () => {
   // Create an application
   const app = new Vue({
-    router: router,
+    router, store,
     // Simply render the router
     render: (h) => { return h("router-view"); }
     // Mount it here
