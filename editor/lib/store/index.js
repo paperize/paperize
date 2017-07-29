530f73e7a2d6b0201ca032302effbb4fcb0c6452
Loren Norman <lorennorman@gmail.com>
2017-07-28 20:34:33 -0400

    better initial load procedure

--- editor/lib/store/index.js
+++ editor/lib/store/index.js
@@ -5,53 +5,70 @@ import auth from '../auth'
 
 Vue.use(Vuex)
 
+const EMPTY_STATE = {
+  idToken: null,
+  authenticated: false,
+  profile: {
+    name: '',
+    avatarSrc: ''
+  },
+  games: []
+}
+
 // LocalStore persistence layer
 let persistence = {
-  loadState: function() {
+  getLocalDB: function() {
+    return JSON.parse(localStorage.getItem("persistence")) || {}
+  },
+
+  setLocalDB: function(localDB) {
+    localStorage.setItem("persistence", JSON.stringify(localDB))
+  },
+
+  loadToken: function() {
     // fetch the JWT from storage
-    let idToken = localStorage.getItem("id_token")
-    // let idToken = 'i am a JWT'
-    let authenticated = !!idToken
-    let profile = {
-      name: '',
-      avatarSrc: ''
-    }
-    let games = []
+    let token = localStorage.getItem("id_token")
 
-    // do we have an active session?
-    if(authenticated) {
-      profile = this.loadProfile(idToken) || profile
-    }
-
-    return { idToken, authenticated, profile, games }
+    return token
   },
 
   loadProfile: function(idToken) {
-    // hydrate the user from the session
-    let localDB = JSON.parse(localStorage.getItem("persistence")) || {}
+    // hydrate the profile from the session
+    let localDB = this.getLocalDB()
+    console.log(localDB)
     let profile = localDB[idToken] && localDB[idToken].profile
 
     return profile
   },
 
-  saveState: function(state) {
-    localStorage.setItem("id_token", state.idToken)
+  saveState: function({ idToken, profile, games }) {
+    if(idToken) {
+      localStorage.setItem("id_token", idToken)
+    } else {
+      localStorage.removeItem("id_token")
+    }
+
+    let localDB = this.getLocalDB()
+
+    if(!localDB[idToken]) {
+      localDB[idToken] = {}
+    }
+
+    localDB[idToken].profile = profile
+    localDB[idToken].games = games
+
+    this.setLocalDB(localDB)
+
   }
   // - hydrate the games collection from the user token
   // games = JSON.load(localStorage.getItem())
 }
 
-// Auth0 Authentication Layer
-auth.on("authenticated", ({ idToken }) => {
-  store.dispatch("loggedInAs", { idToken })
-})
-
 let store = new Vuex.Store({
-  state: persistence.loadState(),
+  state: EMPTY_STATE,
 
   mutations: {
     logout (state) {
-      // mutate state
       state.idToken = null
       state.authenticated = false
       state.profile.name = ''
@@ -64,7 +81,7 @@ let store = new Vuex.Store({
       state.idToken = payload.idToken
       state.authenticated = true
       state.profile.name = payload.profile.name
-      state.profile.avatarSrc = payload.profile.picture
+      state.profile.avatarSrc = payload.profile.avatarSrc
 
       persistence.saveState(state)
     }
@@ -76,6 +93,7 @@ let store = new Vuex.Store({
     },
 
     loggedInAs (context, { idToken }) {
+      console.log('loggedInAs', idToken)
       let profile = persistence.loadProfile(idToken)
 
       let commitProfile = function() {
@@ -88,15 +106,33 @@ let store = new Vuex.Store({
             console.error("Error fetching profile for:", idToken)
             console.error(error.message)
           } else {
+            console.log("Profile fetched from Auth0")
             profile = fetchedProfile
+            profile.avatarSrc = profile.picture
             commitProfile()
           }
         })
       } else {
+        console.log("Profile fetched from localStorage")
         commitProfile()
       }
     }
   }
 })
+
+let idToken = persistence.loadToken()
+
+if(idToken) {
+  // Log in with a persisted token
+  console.log("Logging in from localStorage")
+  store.dispatch("loggedInAs", { idToken })
+
+} else {
+  // Log in with Auth0
+  auth.on("authenticated", ({ idToken }) => {
+    console.log("Logging in from Auth0")
+    store.dispatch("loggedInAs", { idToken })
+  })
+}
 
 export default store
