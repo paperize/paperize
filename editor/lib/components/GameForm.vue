25b9540679d9d9fa7e5cb0075f6758584c0337e7
Loren Norman <lorennorman@gmail.com>
2017-08-04 14:18:24 -0400

    Foundation Reveal dom fix

--- editor/lib/components/GameForm.vue
+++ editor/lib/components/GameForm.vue
@@ -45,6 +45,13 @@
   export default {
     mixins: [FoundationMixin],
 
+    // The way Foundation's Reveal works, it moves the dom element up to be a
+    // direct child of <body>, removing Vue's ability to remove it cleanly upon
+    // destroy. Add a little extra love here to see that the job is really done!
+    destroyed() {
+      $(this.$el).remove()
+    },
+
     props: {
       mode: {
         default: 'edit',
