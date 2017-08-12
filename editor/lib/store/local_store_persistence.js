e1fcf922ddbdfb632e2bd0b0476846b35a29d6bf
Loren Norman <lorennorman@gmail.com>
2017-08-12 14:08:08 -0400

    bump lock lib, add access token storage

--- editor/lib/store/local_store_persistence.js
+++ editor/lib/store/local_store_persistence.js
@@ -2,38 +2,51 @@
 import jwtDecode from 'jwt-decode'
 
 const ID_TOKEN_KEY = 'id_token'
+const ACCESS_TOKEN_KEY = 'access_token'
 const DATABASE_KEY = 'persistence'
 
 export default {
-  tokenToRecordId (idToken) {
+  tokenToRecordId(idToken) {
     // parse the JWT for a globally unique user id
     return jwtDecode(idToken).sub
   },
 
-  getLocalDB () {
+  getLocalDB() {
     return JSON.parse(localStorage.getItem(DATABASE_KEY)) || {}
   },
 
-  setLocalDB (localDB) {
+  setLocalDB(localDB) {
     localStorage.setItem(DATABASE_KEY, JSON.stringify(localDB))
   },
 
-  loadToken () {
+  loadIdToken() {
     // fetch the JWT from storage
-    let token = localStorage.getItem(ID_TOKEN_KEY)
-
-    return token
+    return localStorage.getItem(ID_TOKEN_KEY)
   },
 
-  loadProfile (idToken) {
+  loadAccessToken() {
+    // fetch the JWT from storage
+    return localStorage.getItem(ACCESS_TOKEN_KEY)
+  },
+
+  loadTokens() {
+    // fetch the JWT from storage
+    return { idToken: this.loadIdToken(), accessToken: this.loadAccessToken() }
+  },
+
+  loadRecordId() {
+    return this.tokenToRecordId(this.loadToken())
+  },
+
+  loadProfile(idToken) {
     return this.loadPersisted("profile", idToken)
   },
 
-  loadGames (idToken) {
+  loadGames(idToken) {
     return this.loadPersisted("games", idToken)
   },
 
-  loadPersisted (recordName, idToken) {
+  loadPersisted(recordName, idToken) {
     // get a record ID
     let recordId = this.tokenToRecordId(idToken)
     // load the entire database
@@ -44,11 +57,13 @@ export default {
     return record
   },
 
-  saveState ({ idToken, profile, games }) {
-    if(!idToken) {
+  saveState({ idToken, accessToken, profile, games }) {
+    if(!idToken || !accessToken) {
       localStorage.removeItem(ID_TOKEN_KEY)
+      localStorage.removeItem(ACCESS_TOKEN_KEY)
     } else {
       localStorage.setItem(ID_TOKEN_KEY, idToken)
+      localStorage.setItem(ACCESS_TOKEN_KEY, accessToken)
 
       let recordId = this.tokenToRecordId(idToken)
       let localDB = this.getLocalDB()
