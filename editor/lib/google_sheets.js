078e02102c81bfd637a8c17a4bda8eba7ec9f007
Loren Norman <lorennorman@gmail.com>
2017-08-12 14:11:32 -0400

    google sheet lib starting to work

--- editor/lib/google_sheets.js
+++ editor/lib/google_sheets.js
@@ -1,1 +1,53 @@
-CLIENT_ID = "991093846081-9fps02e3ijk98hpetv0jvpjqm195as2m.apps.googleusercontent.com"	
+import Promise from 'bluebird'
+import auth from './auth'
+
+let user = auth.user
+
+const CLIENT_ID      = "991093846081-9fps02e3ijk98hpetv0jvpjqm195as2m.apps.googleusercontent.com"
+const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const SCOPES         = "https://www.googleapis.com/auth/spreadsheets";
+
+let getSheetsAPI = (token) => {
+  return new Promise((resolve, reject) => {
+    if(gapi.client) {
+      resolve(gapi.client)
+    } else {
+      gapi.load('client:auth2', () => {
+        return gapi.client.init({
+          discoveryDocs: DISCOVERY_DOCS,
+          clientId:      CLIENT_ID,
+          scope:         SCOPES,
+        })
+
+        .then(() => {
+          gapi.client.setToken({access_token: token})
+          resolve(gapi.client)
+        })
+      })
+    }
+  })
+}
+
+let loadSheet = function(token, sheetId, range) {
+  getSheetsAPI(token).then((api) => {
+    api.sheets.spreadsheets.values.get({
+      spreadsheetId: sheetId,
+      range: range,
+    }).then(function(response) {
+      let range = response.result;
+      if (range.values.length > 0) {
+        for (let i = 0; i < range.values.length; i++) {
+          var row = range.values[i];
+          // work with row
+          console.log(row)
+        }
+      } else {
+        console.log('-')
+      }
+    }, function(response) {
+      console.log("Error:", response.result.error.message)
+    });
+  })
+}
+
+export default { loadSheet }
