c3377e045ccef50b8732c287c7f66403d47a0dd4
Loren Norman <lorennorman@gmail.com>
2017-08-27 22:39:44 -0400

    extract a source manager component

--- editor/lib/components/ComponentEditor.vue
+++ editor/lib/components/ComponentEditor.vue
@@ -4,43 +4,8 @@ div
   hr
 
   .grid-x
-    #source-manager.small-4
-      h5 Source Manager
-      hr
-
-      div(v-if="activeSource")
-        a(@click="unsetSource({ component })") Change source
-        p Source: {{ activeSource.name }}
-
-        p Properties:
-
-        ul
-          li(v-for="property in activeSourceProperties")
-            strong {{ property }}
-
-            p
-              | e.g.:
-              em  {{ activeSourcePropertyExamples(property) }}
-
-      div(v-else)
-        p(v-if="sources.length == 0")
-          | You have no sources.
-        div(v-else)
-          p Select a Source:
-          ul
-            li(v-for="source in sources")
-              a(@click="setComponentSource({ component, source })") {{ source.name }}
-
-
-        a.button(data-open="source-paste-form") Paste a Link
-
-        source-paste-form
-
-        a.button(data-open="source-explorer") Browse Google Sheets
-        .reveal#source-explorer(data-reveal)
-          h2 Browse Your Google Sheets
-
-          p Loading your Sheets...
+    .small-4
+      component-source-manager(:component="component")
 
     #transform-manager.small-4
       h5 Transform Manager
@@ -53,23 +18,13 @@ div
 
 <script>
   import { mapState, mapGetters, mapMutations, mapActions, } from 'vuex'
-  import SourcePasteForm from './SourcePasteForm.vue'
+  import ComponentSourceManager from './ComponentSourceManager.vue'
 
   export default {
     props: ["component"],
 
-    computed: {
-      ...mapState(["sources"]),
-      ...mapGetters(["activeSource", "activeSourceProperties", "activeSourcePropertyExamples"]),
-    },
-
-    methods: {
-      ...mapActions(["setComponentSource"]),
-      ...mapMutations(["unsetSource"])
-    },
-
     components: {
-      'source-paste-form': SourcePasteForm
+      'component-source-manager': ComponentSourceManager
     }
   }
 </script>
