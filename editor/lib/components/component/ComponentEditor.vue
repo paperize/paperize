0ace9830c6c180a669fdcce4af49212c00251704
Loren Norman <lorennorman@gmail.com>
2017-08-28 13:32:22 -0400

    split out the transform manager for good measure

--- editor/lib/components/component/ComponentEditor.vue
+++ editor/lib/components/component/ComponentEditor.vue
@@ -7,39 +7,25 @@ div
     .small-4
       source-manager(:component="component")
 
-    #transform-manager.small-4
-      h5 Transform Manager
-      hr
-
-      p Transforms:
-      table(v-if="source")
-        thead
-          th Name:
-          th Type:
-        tr(v-for="property in sourceProperties(source)")
-          td Expose("{{ property }}")
-          td Raw
+    .small-4
+      transform-manager(:component="component")
 
     .small-4
       template-manager(:component="component")
 </template>
 
 <script>
-  import { mapState, mapGetters, mapMutations, mapActions, } from 'vuex'
   import SourceManager from '../source/SourceManager.vue'
   import TemplateManager from '../template/TemplateManager.vue'
+  import TransformManager from '../transform/TransformManager.vue'
 
   export default {
     props: ["component"],
 
-    computed: {
-      ...mapGetters(["sourceProperties"]),
-      source() { return this.component.source }
-    },
-
     components: {
       'source-manager': SourceManager,
-      'template-manager': TemplateManager
+      'template-manager': TemplateManager,
+      'transform-manager': TransformManager
     }
   }
 </script>
---
+++ editor/lib/components/transform/TransformManager.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+#transform-manager
+  h5 Transform Manager
+  hr
+
+  p Transforms:
+  table(v-if="source")
+    thead
+      th Name:
+      th Type:
+    tr(v-for="property in sourceProperties(source)")
+      td Expose("{{ property }}")
+      td Raw
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    props: ["component"],
+
+    computed: {
+      ...mapGetters(["sourceProperties"]),
+      source() { return this.component.source }
+    }
+  }
+</script>
