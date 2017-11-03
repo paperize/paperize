bc31ed146572e3fff1bb108a0429246f3c989fbe
Loren Norman <lorennorman@gmail.com>
2017-11-03 11:27:54 -0400

    set GAPI source conditional to environment

--- editor/webpack.config.js
+++ editor/webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack')
   , CopyWebpackPlugin = require('copy-webpack-plugin')
   , path = require('path')
 
+
 module.exports = {
   entry: './lib/main.js',
   output: {
@@ -11,7 +12,23 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin({'NODE_ENV': 'development'}),
     new CopyWebpackPlugin([
-      { context: 'static', from: '**'}
+      { context: 'static', from: '**', transform: function(content, path) {
+        // Set local/remote JS includes in the HTML based on environment
+        if(path.includes("/static/index.html")) {
+          var gapiInclude = ""
+
+          if(process.env.NODE_ENV === 'production') {
+            gapiInclude = "https://apis.google.com/js/api.js"
+          } else {
+            gapiInclude = "js/vendor/gapi.min.js"
+          }
+
+          return content.toString().replace("GAPI_SOURCE", gapiInclude)
+
+        } else {
+          return content
+        }
+      }}
     ])
   ],
   module: {
