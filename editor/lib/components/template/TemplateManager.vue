43976bdbb34355881dae03e62b59493b63d84355
Loren Norman <lorennorman@gmail.com>
2017-11-28 14:16:18 -0500

    enable preset vs custom page sizes

--- editor/lib/components/template/TemplateManager.vue
+++ editor/lib/components/template/TemplateManager.vue
@@ -1,22 +1,42 @@
 <template lang="pug">
 #template-manager
   .grid-x
-    .small-1.cell
-      a.unset-source(@click="unsetSource({ component })") &times;
-
-    .small-11.cell
-      h5.truncate "Raw Source Viewer"
+    .small-12.cell
+      h5.truncate PDF Preview
 
     .small-10.small-offset-1.cell(v-if="component.source")
       .grid-x.grid-margin-x
-        .small-2.cell
-          label Width:
-        .small-4.cell
-          input(type="text" v-model="componentPageWidth")
-        .small-2.cell
-          label Height:
-        .small-4.cell
-          input(type="text" v-model="componentPageHeight")
+        .small-12.cell
+          ul.menu.horizontal
+            li Page:
+            li
+              a(@click="setPageMode('preset')") Preset
+            li
+              a(@click="setPageMode('custom')") Custom
+
+        template(v-if="pageMode === 'preset'")
+          .small-5.cell
+            label Format:
+            select(v-model="componentPageWidth")
+              option(v-for="pageFormat in pageFormats" :value="pageFormat")
+                | {{ capitalize(pageFormat.replace("-", " ")) }}
+
+          .small-7.cell
+            label Orientation:
+            select(v-model="componentPageHeight")
+              option(value="portrait") Portrait
+              option(value="landscape") Landscape
+
+        template(v-else-if="pageMode === 'custom'")
+          .small-6.cell
+            label Width:
+            input(type="number" v-model="componentPageWidth")
+
+          .small-6.cell
+            label Height:
+            input(type="number" v-model="componentPageHeight")
+
+      hr
 
       ul.menu.horizontal
         li
@@ -25,13 +45,11 @@
           | Item {{ currentItemIndex }} / {{ totalItems }}
         li
           a(@click="nextItem") &gt;&gt;
+        li
+          a.button.tiny(@click="$modal.show('Template Preview')") Show
 
       .card
-        //- .card-section(v-for="property in currentItem")
-        //-   strong {{ property.key }}:
-        //-   |  {{ property.value }}
-
-        iframe(:src="pdfBlob")
+        template-preview.inline-preview(:game="activeGame" :component="activeComponent" :item="currentItem")
 
       ul.menu.horizontal
         li
@@ -40,39 +58,42 @@
           | Item {{ currentItemIndex }} / {{ totalItems }}
         li
           a(@click="nextItem") &gt;&gt;
+
+      modal(name="Template Preview" width="90%" height="90%")
+        template-preview.modal-preview(:game="activeGame" :component="activeComponent" :item="currentItem")
 
     p(v-else) Select a Source...
 </template>
 
 <script>
   import _ from 'lodash'
-  import pdfRenderer from './template_examples'
+  import { mapGetters } from 'vuex'
+
+  import TemplatePreview from './TemplatePreview.vue'
 
   export default {
     props: ["component"],
 
+    components: {
+      "template-preview": TemplatePreview
+    },
+
     data() {
       return {
         currentItemIndex: 1,
-        pdfBlob: null
-      }
-    },
-
-    watch: {
-      componentPageWidth() {
-        this.calculatePDFBlob()
-      },
-
-      componentPageHeight() {
-        this.calculatePDFBlob()
-      },
-
-      currentItem() {
-        this.calculatePDFBlob()
+        pdfBlob: null,
+        pageMode: "preset",
+        pageFormats: ['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
+          'a9', 'a10', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8',
+          'b9', 'b10', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8',
+          'c9', 'c10', 'dl', 'letter', 'government-letter', 'legal',
+          'junior-legal', 'ledger', 'tabloid', 'credit-card']
       }
     },
 
     computed: {
+      ...mapGetters(["activeGame", "activeComponent"]),
+
       componentPageWidth: {
         get() { return this.component.pageSize.w },
         set(newWidth) {
@@ -107,6 +128,18 @@
     },
 
     methods: {
+      capitalize: _.capitalize,
+
+      setPageMode(newPageMode) {
+        this.pageMode = newPageMode
+
+        if(this.pageMode === "preset") {
+
+        } else if(this.pageMode === "custom") {
+
+        }
+      },
+
       previousItem() {
         this.currentItemIndex = Math.max(this.currentItemIndex - 1, 1)
       },
@@ -114,22 +147,17 @@
       nextItem() {
         this.currentItemIndex = Math.min(this.currentItemIndex + 1, this.totalItems)
       },
-
-      calculatePDFBlob() {
-        let game = this.$store.getters.activeGame
-        let component = this.$store.getters.activeComponent
-        let item = this.currentItem
-
-        pdfRenderer.renderItemToPdf(game, component, item).then((pdfData) => {
-          this.pdfBlob = pdfData
-        })
-      }
     }
   }
 </script>
 
 <style>
-  iframe {
+  .modal-preview {
+    width: 100%;
+    height: 100%;
+  }
+
+  .inline-preview {
     min-height: 400px;
   }
 </style>
