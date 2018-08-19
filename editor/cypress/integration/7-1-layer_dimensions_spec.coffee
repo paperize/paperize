410a91591cf40f3797ba2aa7db80cb973375cd25
Loren Norman <lorennorman@gmail.com>
2018-08-19 13:06:57 -0400

    toggle between XYWH and Inset layout modes

--- editor/cypress/integration/7-1-layer_dimensions_spec.coffee
+++ editor/cypress/integration/7-1-layer_dimensions_spec.coffee
@@ -7,9 +7,32 @@ describe "Layer Dimensions", ->
     cy.contains("New Layer").click()
     cy.contains("Shape").click()
 
-  it.only "let's me toggle between coord/dims and inset mode", ->
-    # see the x/y/w/h percent values
-    # click inset
-    # see the t/r/b/l percent values
-    # click coord/dim
-    # see the x/y/w/h percent values
+  it.only "let's me toggle between xywh and inset mode", ->
+    cy.get("#dimension-editor").within ->
+      # see the x/y/w/h percent values
+      cy.get("#dimension-x").invoke("val").should("eq", "5")
+      cy.get("#dimension-y").invoke("val").should("eq", "5")
+      cy.get("#dimension-w").invoke("val").should("eq", "90")
+      cy.get("#dimension-h").invoke("val").should("eq", "90")
+      # click inset
+      cy.contains("Inset").click()
+      # see the t/r/b/l percent values
+      cy.get("#dimension-t").invoke("val").should("eq", "5")
+      cy.get("#dimension-t").clear().type("10")
+      cy.wait(150)
+      cy.get("#dimension-r").invoke("val").should("eq", "5")
+      cy.get("#dimension-r").clear().type("10")
+      cy.wait(150)
+      cy.get("#dimension-b").invoke("val").should("eq", "5")
+      cy.get("#dimension-b").clear().type("10")
+      cy.wait(150)
+      cy.get("#dimension-l").invoke("val").should("eq", "5")
+      cy.get("#dimension-l").clear().type("10")
+      cy.wait(150)
+      # click xywh
+      cy.contains("XYWH").click()
+      # see the x/y/w/h percent values
+      cy.get("#dimension-x").invoke("val").should("eq", "10")
+      cy.get("#dimension-y").invoke("val").should("eq", "10")
+      cy.get("#dimension-w").invoke("val").should("eq", "80")
+      cy.get("#dimension-h").invoke("val").should("eq", "80")
--- editor/lib/components/template/layer/DimensionEditor.vue
+++ editor/lib/components/template/layer/DimensionEditor.vue
@@ -1,34 +1,69 @@
 <template lang="pug">
-fieldset.fieldset
+fieldset#dimension-editor.fieldset
   legend Dimensions
+
+  ul.menu
+    li.menu-text Layout Mode
+    li(:class="{'is-active': modeXYWH }")
+      a(@click="dimensionMode = 'xywh'") XYWH
+    li(:class="{'is-active': modeInset }")
+      a(@click="dimensionMode = 'inset'") Inset
 
   p Expressed as percentage of total width or height.
 
-  .grid-x.grid-padding-x
-    .medium-12.large-6.cell
-      .input-group
-        span.input-group-label
-          strong X%
-        input.input-group-field(type="number" v-model.number="layerDimensionX")
+  template(v-if="modeXYWH")
+    .grid-x.grid-padding-x
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-x")
+            strong X%
+          input#dimension-x.input-group-field(type="number" v-model.number="dimensionX")
 
-    .medium-12.large-6.cell
-      .input-group
-        span.input-group-label
-          strong Y%
-        input.input-group-field(type="number" v-model.number="layerDimensionY")
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-y")
+            strong Y%
+          input#dimension-y.input-group-field(type="number" v-model.number="dimensionY")
 
-  .grid-x.grid-padding-x
-    .medium-12.large-6.cell
-      .input-group
-        span.input-group-label
-          strong W%
-        input.input-group-field(type="number" v-model.number="layerDimensionW")
+    .grid-x.grid-padding-x
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-w")
+            strong W%
+          input#dimension-w.input-group-field(type="number" v-model.number="dimensionW")
 
-    .medium-12.large-6.cell
-      .input-group
-        span.input-group-label
-          strong H%
-        input.input-group-field(type="number" v-model.number="layerDimensionH")
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-h")
+            strong H%
+          input#dimension-h.input-group-field(type="number" v-model.number="dimensionH")
+
+  template(v-else-if="modeInset")
+    .grid-x.grid-padding-x
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-t")
+            strong Top%
+          input#dimension-t.input-group-field(type="number" v-model.number="dimensionT")
+
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-r")
+            strong Right%
+          input#dimension-r.input-group-field(type="number" v-model.number="dimensionR")
+
+    .grid-x.grid-padding-x
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-b")
+            strong Bottom%
+          input#dimension-b.input-group-field(type="number" v-model.number="dimensionB")
+
+      .medium-12.large-6.cell
+        .input-group
+          label.input-group-label(for="dimension-l")
+            strong Left%
+          input#dimension-l.input-group-field(type="number" v-model.number="dimensionL")
 </template>
 
 <script>
@@ -40,44 +75,93 @@ fieldset.fieldset
   export default {
     props: ["layer"],
 
+    data() {
+      return {
+        dimensionMode: 'xywh'
+      }
+    },
+
     computed: {
       ...mapGetters(["getLayerDimensions"]),
 
-      layerDimensions() { return this.getLayerDimensions(this.layer) },
+      modeXYWH() { return this.dimensionMode == 'xywh'},
+      modeInset() { return this.dimensionMode == 'inset'},
 
-      layerDimensionX: {
-        get() { return this.layerDimensions.x },
+      dimensions() { return this.getLayerDimensions(this.layer) },
+
+      dimensionX: {
+        get() { return this.dimensions.x },
 
         set(newX) {
           if(isString(newX) || newX < 0) { newX = 0 }
-          this.updateDimensionSlowly({ ...this.layerDimensions, x: newX })
+          this.updateDimensionSlowly({ ...this.dimensions, x: newX })
         }
       },
 
-      layerDimensionY: {
-        get() { return this.layerDimensions.y },
+      dimensionY: {
+        get() { return this.dimensions.y },
 
         set(newY) {
           if(isString(newY) || newY < 0) { newY = 0 }
-          this.updateDimensionSlowly({ ...this.layerDimensions, y: newY })
+          this.updateDimensionSlowly({ ...this.dimensions, y: newY })
         }
       },
 
-      layerDimensionW: {
-        get() { return this.layerDimensions.w },
+      dimensionW: {
+        get() { return this.dimensions.w },
 
         set(newW) {
           if(isString(newW) || newW < 0) { newW = 0 }
-          this.updateDimensionSlowly({ ...this.layerDimensions, w: newW })
+          this.updateDimensionSlowly({ ...this.dimensions, w: newW })
         }
       },
 
-      layerDimensionH: {
-        get() { return this.layerDimensions.h },
+      dimensionH: {
+        get() { return this.dimensions.h },
 
         set(newH) {
           if(isString(newH) || newH < 0) { newH = 0 }
-          this.updateDimensionSlowly({ ...this.layerDimensions, h: newH })
+          this.updateDimensionSlowly({ ...this.dimensions, h: newH })
+        }
+      },
+
+      dimensionT: {
+        get() { return this.dimensions.y },
+
+        set(newT) {
+          if(isString(newT) || newT < 0) { newT = 0 }
+          let newHeight = (this.dimensions.h + this.dimensions.y) - newT
+          this.updateDimensionSlowly({ ...this.dimensions, y: newT, h: newHeight })
+        }
+      },
+
+      dimensionR: {
+        get() { return (100 - this.dimensions.x - this.dimensions.w) },
+
+        set(newR) {
+          if(isString(newR) || newR < 0) { newR = 0 }
+          let newWidth = 100 - newR - this.dimensions.x
+          this.updateDimensionSlowly({ ...this.dimensions, w: newWidth })
+        }
+      },
+
+      dimensionB: {
+        get() { return (100 - this.dimensions.y - this.dimensions.h) },
+
+        set(newB) {
+          if(isString(newB) || newB < 0) { newB = 0 }
+          let newHeight = 100 - newB - this.dimensions.y
+          this.updateDimensionSlowly({ ...this.dimensions, h: newHeight })
+        }
+      },
+
+      dimensionL: {
+        get() { return this.dimensions.x },
+
+        set(newL) {
+          if(isString(newL) || newL < 0) { newL = 0 }
+          let newWidth = (this.dimensions.w + this.dimensions.x) - newL
+          this.updateDimensionSlowly({ ...this.dimensions, x: newL, w: newWidth })
         }
       },
     },
@@ -91,3 +175,9 @@ fieldset.fieldset
     }
   }
 </script>
+
+<style>
+  .menu {
+    font-size: .8em;
+  }
+</style>
