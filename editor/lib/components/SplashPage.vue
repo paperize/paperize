e3c0a9f2c99c03426351d9ec88381a76b3bb4306
Loren Norman <lorennorman@gmail.com>
2018-11-06 14:44:27 -0500

    simplify splash page

--- editor/lib/components/SplashPage.vue
+++ editor/lib/components/SplashPage.vue
@@ -1,54 +1,17 @@
 <template lang="pug">
-div
-  v-container(fluid)
-    v-layout(row wrap)
-      v-flex(xs12 md5)
-        v-layout(column)
-          v-flex
-            .headline Benefits
-            hr
+v-layout(row fluid)
+  v-flex(xs6)
+    .headline Get Started!
 
-            .body-1 All about benefits
+    v-btn(color="success") Sign in with Google
+  v-flex(xs6)
+    .headline What is Paperize?
 
-          v-flex
-            .headline Benefits
-            hr
+    p Paperize is the free-as-in-free tabletop game prototyping tool for kids, teachers, facilitators, and game design professionals.
 
-            .body-1 All about benefits
-
-          v-flex
-            .headline Benefits
-            hr
-
-            .body-1 All about benefits
-
-      v-spacer()
-
-      v-flex(xs12 md6 elevation-2 @click="getStarted")
-        v-layout(column)
-          v-flex
-            .headline Get Started
-          v-flex
-            .body-2 Getting started is really wonderful and you should totally today!
-
-            v-btn(color="success") Get Started
-
-  v-container(fluid grid-list-lg)
-    v-layout(row wrap)
-      v-flex(xs12 md4)
-        .headline About the Project
-        hr
-
-        vue-embed(v-if="notInTest" id="gist" :options="{ emoji: true }")
-          p.gist https://gist.github.com/lorennorman/9d0f3d7df597756a3bc14de4288e7c45.js
-
-      v-flex(sm12 md4)
-        .headline Free as in Free
-        hr
-
-      v-flex(sm12 md4)
-        .headline Community Driven
-        hr
+    v-btn(href="https://patreon.com/paperize") Learn More
+//- vue-embed(v-if="notInTest" id="gist" :options="{ emoji: true }")
+//-   p.gist https://gist.github.com/lorennorman/9d0f3d7df597756a3bc14de4288e7c45.js
 </template>
 
 <script>
