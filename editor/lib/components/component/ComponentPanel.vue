b8e0c6a0ee825c863747e314b1f605889e592995
Loren Norman <lorennorman@gmail.com>
2018-12-12 10:17:37 -0500

    break ComponentForm into NewComponentForm and EditComponentForm

--- editor/lib/components/component/ComponentPanel.vue
+++ editor/lib/components/component/ComponentPanel.vue
@@ -3,27 +3,32 @@ v-layout(column).component-panel
   v-flex(sm12)
     .headline Components
 
-    v-dialog(v-model="showEditDialog" max-width="500" lazy)
-      v-btn(small slot="activator" @click="editComponent = {}") New Component
-      component-form(@close-dialog="showEditDialog = false" :component="editComponent")
+    v-dialog(v-model="showNewDialog" max-width="500" lazy)
+      v-btn(small slot="activator") New Component
+      new-component-form(@close-dialog="showNewDialog = false")
 
-  component-card(v-for="component in components" :key="component.id" :component="component" @edit-me="showEditDialog = true; editComponent = component")
+    v-dialog(v-model="showEditDialog" max-width="500" lazy)
+      edit-component-form(:component="editComponent" @close-dialog="showEditDialog = false")
+
+  component-card(v-for="component in components" :key="component.id" :component="component" @edit-me="editComponent = component; showEditDialog = true")
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
   import ComponentCard from './ComponentCard.vue'
-  import ComponentForm from './ComponentForm.vue'
+  import NewComponentForm from './NewComponentForm.vue'
+  import EditComponentForm from './EditComponentForm.vue'
 
   export default {
     props: ["components"],
 
-    components: { ComponentCard, ComponentForm },
+    components: { ComponentCard, NewComponentForm, EditComponentForm },
 
     data() {
       return {
-        editComponent: {},
-        showEditDialog: false
+        showNewDialog: false,
+        showEditDialog: false,
+        editComponent: null
       }
     },
   }
