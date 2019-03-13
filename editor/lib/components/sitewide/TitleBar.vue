0d69b3ef6f2c5d8c0540a4861b7303c917790f5f
Loren Norman <lorennorman@gmail.com>
2019-03-13 14:40:43 -0400

    bump to 5.5

--- editor/lib/components/sitewide/TitleBar.vue
+++ editor/lib/components/sitewide/TitleBar.vue
@@ -4,7 +4,7 @@ v-toolbar(app)
     router-link(:to="{ name: homeLink }") Paperize.io
 
     v-tooltip
-      span.caption(slot="activator")= " ver.A5.4"
+      span.caption(slot="activator")= " ver.A5.5"
       | Alpha 5 "Reclusive Scrivener " {{ gitSha }}
 
   v-spacer
