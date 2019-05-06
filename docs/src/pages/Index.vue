c89cc02e158b30178037ceffd3ee43216f9ae1c2
Loren Norman <lorennorman@gmail.com>
2019-05-06 13:13:25 -0400

    rewrite the docs homepage

--- docs/src/pages/Index.vue
+++ docs/src/pages/Index.vue
@@ -2,11 +2,26 @@
   layout
     v-layout()
       v-flex(xs-12)
-        h1 Paperize
+        h1 Paperize: <em>create better games, faster!</em>
 
-        em Create better games, faster!
+        h2 Paperize is currently in Alpha status
+        p This means that it:
+        p
+          ul
+            li is missing major features
+            li has unacceptable bugs
+            li is not easy to understand the user interface
+        p If you'd like to see it progress faster...
 
-        p Paperize is currently in Alpha, which means that it is missing major features, has unacceptable bugs, and the user interface is hard to understand.
+        h2 Consider <a href="https://patreon.com/paperize">Becoming a Patron!</a>
+        p Paperize is <strong>solely supported by generous donations from visionary game designers</strong>: no fees, no spying, no selling your data.
+
+        h2 Read the <a href="/guides/getting-started">Getting Started Guide</a>
+        p This is a great way to rapidly understand the workflow Paperize enables.
+
+        h2
+          a(href="https://beta.editor.paperize.io") Visit the Application
+        p Paperize is ready when you are!
 </template>
 
 <script>
