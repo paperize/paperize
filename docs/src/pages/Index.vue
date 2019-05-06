a84366fd8b3f0cf9d0a2bd5c1d8d9abeb19e4baf
Loren Norman <lorennorman@gmail.com>
2019-05-06 16:27:12 -0400

    discord link, target=blank all the links

--- docs/src/pages/Index.vue
+++ docs/src/pages/Index.vue
@@ -13,15 +13,18 @@
             li is not easy to understand the user interface
         p If you'd like to see it progress faster...
 
-        h2 Consider <a href="https://patreon.com/paperize">Becoming a Patron!</a>
+        h2 Consider <a href="https://patreon.com/paperize" target="_blank">Becoming a Patron!</a>
         p Paperize is <strong>solely supported by generous donations from visionary game designers</strong>: no fees, no spying, no selling your data.
 
         h2 Read the <a href="/guides/getting-started">Getting Started Guide</a>
         p This is a great way to rapidly understand the workflow Paperize enables.
 
         h2
-          a(href="https://beta.editor.paperize.io") Visit the Application
+          a(href="https://beta.editor.paperize.io" target="_blank") Visit the Application
         p Paperize is ready when you are!
+
+        h2 Want Live Chat Help? <a href="https://discord.gg/9ggkkQp" target="_blank">Join the Discord</a>
+        p On the Paperize Discord you'll find a community of game designers working on their games and helping each other. The lead developer monitors it as well, helping users find their way, fixing bugs, and taking feature requests.
 </template>
 
 <script>
