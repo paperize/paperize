bad2c692924865b66f634e17fcbe6128864e2321
Loren Norman <lorennorman@gmail.com>
2019-06-08 22:27:44 -0400

    drop in a font cache get axios on my (promise) level

--- editor/lib/services/pdf_renderer/fonts/font_loader.js
+++ editor/lib/services/pdf_renderer/fonts/font_loader.js
@@ -1,21 +1,35 @@
 import jsPDF from 'jspdf'
 import axios from 'axios'
 import { includes } from 'lodash'
+import { getCachedFont, setCachedFont } from '../../font_cache'
 
 const addedFontLocations = []
 
 export function preloadFonts(fonts) {
   return Promise.each(fonts, (font) => {
-    // don't do this if we've already done it
-    if(includes(addedFontLocations, font.location)) {
+    // bail if this font has already been loaded into jspdf
+    if(!font.location || includes(addedFontLocations, font.location)) {
       return
     }
 
-    // font has not been loaded
-    return urlToBase64(font.location)
+    // check the cache first
+    return getCachedFont(font.location)
       .then((fontBase64) => {
+        if(fontBase64) {
+          // cache hit, move on!
+          return fontBase64
+        } else {
+          // cache miss, actually do the work
+          return urlToBase64(font.location)
+            // set cache
+            .tap((fontBase64) => setCachedFont(font.location, fontBase64))
+        }
+      })
+
+      .then((fontBase64) => {
+        // do the work
         pushFontIntoJsPDF(font.family, font.variant, fontBase64)
-        // remember we did this so we never do it again
+        // and remember it
         addedFontLocations.push(font.location)
       })
   })
@@ -23,7 +37,7 @@ export function preloadFonts(fonts) {
 
 // Black magic I tell you.
 function urlToBase64(url) {
-  return axios.get(url, { responseType: 'blob' })
+  return Promise.try(() => axios.get(url, { responseType: 'blob' })
     .then((response) => response.data)
 
     .then((blob) => new Promise((resolve) => {
@@ -37,6 +51,7 @@ function urlToBase64(url) {
         resolve(base64Font)
       }
     }))
+  )
 }
 
 export function pushFontIntoJsPDF(fontName, fontVariant, fontBase64) {
