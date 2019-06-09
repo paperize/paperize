07ca0bb2579312a70a1849cb65d8132719672b7a
Loren Norman <lorennorman@gmail.com>
2019-06-08 21:27:45 -0400

    remember what font's we've loaded into jsPDF already and don't repeat

--- editor/lib/services/pdf_renderer/fonts/font_loader.js
+++ editor/lib/services/pdf_renderer/fonts/font_loader.js
@@ -1,25 +1,27 @@
 import jsPDF from 'jspdf'
 import axios from 'axios'
+import { includes } from 'lodash'
 
-export function loadFont(fontName, fontVariant, fontBase64) {
-  var callAddFont = function () {
-    const fontIndexName = `${fontName}-${fontVariant}.ttf`
-    this.addFileToVFS(fontIndexName, fontBase64)
-    this.addFont(fontIndexName, fontName, fontVariant)
-  }
-
-  jsPDF.API.events.push(['addFonts', callAddFont])
-}
+const addedFontLocations = []
 
 export function preloadFonts(fonts) {
   return Promise.each(fonts, (font) => {
+    // don't do this if we've already done it
+    if(includes(addedFontLocations, font.location)) {
+      return
+    }
+
+    // font has not been loaded
     return urlToBase64(font.location)
       .then((fontBase64) => {
-        loadFont(font.family, font.variant, fontBase64)
+        pushFontIntoJsPDF(font.family, font.variant, fontBase64)
+        // remember we did this so we never do it again
+        addedFontLocations.push(font.location)
       })
   })
 }
 
+// Black magic I tell you.
 function urlToBase64(url) {
   return axios.get(url, { responseType: 'blob' })
     .then((response) => response.data)
@@ -29,11 +31,20 @@ function urlToBase64(url) {
       reader.readAsDataURL(blob)
       reader.onloadend = () => {
         // Remove the DataURL preamble, like:
-        // data:font/ttf;base64
+        // data:font/ttf;base64,
         // data:application/x-font-ttf;base64,
-        // data:text/plain;base64,
         const base64Font = reader.result.replace(/^data:(.+)base64,/, "")
         resolve(base64Font)
       }
     }))
+}
+
+export function pushFontIntoJsPDF(fontName, fontVariant, fontBase64) {
+  var callAddFont = function () {
+    const fontIndexName = `${fontName}-${fontVariant}.ttf`
+    this.addFileToVFS(fontIndexName, fontBase64)
+    this.addFont(fontIndexName, fontName, fontVariant)
+  }
+
+  jsPDF.API.events.push(['addFonts', callAddFont])
 }
