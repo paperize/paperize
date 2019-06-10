ffa7732b0a8176d13e9f05267561284a55f27697
Loren Norman <lorennorman@gmail.com>
2019-06-10 01:08:42 -0400

    little magic property input talker thingie

---
+++ editor/lib/components/source/MagicPropertyInputTalker.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  v-tooltip(top)
+    P Magic Property: {{ magicProperty }}
+    | {{ statusMessage }}
+    v-icon(slot="activator" :color="statusColor" @click="dialogOpen = true") mdi-table-search
+
+    v-dialog(v-model="dialogOpen")
+      v-card
+        v-card-title Moar Infarmation!
+</template>
+
+<script>
+  export default {
+    props: ["layer", "attributeName"],
+
+    data() {
+      return {
+        dialogOpen: false
+      }
+    },
+
+    computed: {
+      magicProperty() { return `${this.layer.name}:${this.attributeName}` },
+      statusColor() { return "rgb(200, 200, 200)" },
+      statusMessage() { return "(Click to learn more)"}
+    },
+
+    methods: {
+
+    }
+  }
+</script>
