b433a6a95517301b12de39e28963c1fbf9a1674c
Loren Norman <lorennorman@gmail.com>
2019-06-15 13:16:05 -0400

    try full-width template editor instead of fullscreen

--- editor/lib/components/template/TemplatePanel.vue
+++ editor/lib/components/template/TemplatePanel.vue
@@ -19,7 +19,7 @@ v-flex#template-editor(sm8 md6)
       p
         em You need to set a data Source before you can get started with Templates.
 
-  v-dialog(v-model="showTemplateManager" fullscreen lazy)
+  v-dialog(v-model="showTemplateManager" full-width lazy)
     template-manager(:component="component" @close-dialog="shutdownTemplateManager()")
 </template>
 
