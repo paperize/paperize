5538f90943234c6bc6e2964f77adaeff0e235239
Loren Norman <lorennorman@gmail.com>
2019-11-11 14:07:31 -0500

    catch failures to load dev keys and give helpful prompt

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,25 @@
-/* global require module */
+/* global require module process */
 const
   merge = require('webpack-merge'),
   common = require('./webpack.common.js'),
-  KEYS = require('./.api_keys.dev.json')
+  KEY_FILE_LOCATION = './.api_keys.dev.json'
+
+let KEYS
+
+// Make a nice help message for new contributors who haven't set up their API Keys yet
+try {
+  KEYS = require(KEY_FILE_LOCATION)
+} catch(error) {
+  if(error.message.indexOf(`Cannot find module '${KEY_FILE_LOCATION}'`) > -1) {
+    console.error(`Error:
+      Expected to find an API key file at ${KEY_FILE_LOCATION}
+      Did you complete the API Keys step in the README?
+    `)
+    process.exit(1)
+  } else {
+    throw error
+  }
+}
 
 module.exports = (env={}) => {
   env.KEYS = KEYS
