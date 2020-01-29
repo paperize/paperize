0a81446b871e82e04e5402c7626ccce8778706bb
Loren Norman <lorennorman@gmail.com>
2020-01-29 14:44:43 -0500

    remove picker bits from DebugMenu, don't need

--- lib/components/sitewide/DebugMenu.vue
+++ lib/components/sitewide/DebugMenu.vue
@@ -7,17 +7,10 @@ v-card
   v-card-title File Upload Test
   v-list
     v-list-tile
-
-  v-card-title Picker Stuff
-  v-list
-    v-list-tile
-      v-btn(@click="openPicker") Open Picker
 </template>
 
 <script>
   import { mapActions, mapGetters } from 'vuex'
-  import { openDebugPicker } from '../../services/google/picker.js'
-  import drive from '../../services/google/drive.js'
 
   export default {
     data: () => {
@@ -31,22 +24,6 @@ v-card
 
     methods: {
       ...mapActions([ ]),
-
-      openPicker() {
-        openDebugPicker((picker) => {
-          picker.enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
-          picker.addView(new google.picker.DocsUploadView())
-          picker.addView(new google.picker.DocsView()
-            .setIncludeFolders(true))
-        })
-
-        .then((pickedFiles) => {
-          console.log(pickedFiles)
-          return drive.getRecord(pickedFiles[0].id)
-        })
-
-        // .then(console.log)
-      }
     }
   }
 </script>
