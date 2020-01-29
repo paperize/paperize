8f8a891686f9ffe3ce68c93e4849693fe0b16682
Loren Norman <lorennorman@gmail.com>
2020-01-29 15:40:14 -0500

    WIP trying `vue-upload-component` lib

--- lib/components/sitewide/DebugMenu.vue
+++ lib/components/sitewide/DebugMenu.vue
@@ -1,22 +1,30 @@
 <template lang="pug">
 v-card
   v-toolbar
-
     v-toolbar-title.text-xs-center Debugger
 
   v-card-title File Upload Test
   v-list
     v-list-tile
+      file-upload(v-model="files" accept="image/*" :multiple="true") Upload Files
+
+    v-list-tile
+      ul
+        li(v-for="file in files") {{file.name}} - Error: {{file.error}}, Success: {{file.success}}
 </template>
 
 <script>
+  import VueUploadComponent from 'vue-upload-component'
   import { mapActions, mapGetters } from 'vuex'
 
   export default {
+    components: {
+      "file-upload": VueUploadComponent
+    },
+
     data: () => {
       return {
-        permissionId: null,
-        recordId: null
+        files: []
       }
     },
 
