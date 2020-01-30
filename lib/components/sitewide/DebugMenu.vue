c8d9ba714cee9d6c2af406f73277f8e1ab5c004d
Loren Norman <lorennorman@gmail.com>
2020-01-29 22:50:24 -0500

    crappy multi-uploader, uploads only last file

--- lib/components/sitewide/DebugMenu.vue
+++ lib/components/sitewide/DebugMenu.vue
@@ -3,19 +3,27 @@ v-card
   v-toolbar
     v-toolbar-title.text-xs-center Debugger
 
-  v-card-title File Upload Test
+  v-card-title File Upload Test {{ anyFiles }}
   v-list
     v-list-tile
-      file-upload(v-model="files" accept="image/*" :multiple="true") Upload Files
+      v-list-tile-content
+        file-upload(v-model="files" accept="image/*" :multiple="true")
+          v-btn(v-if="anyFiles" color="info" ) Add More Files
+          v-btn(v-else color="success") Choose Files to Upload
+
+    v-list-tile(v-if="anyFiles")
+      v-list-tile-content
+        v-btn(color="success" @click="performUpload") Perform Upload
 
     v-list-tile
-      ul
-        li(v-for="file in files") {{file.name}} - Error: {{file.error}}, Success: {{file.success}}
+      v-list
+        v-list-tile(v-for="file in files") {{ file.name }}
 </template>
 
 <script>
   import VueUploadComponent from 'vue-upload-component'
   import { mapActions, mapGetters } from 'vuex'
+  import drive from '../../services/google/drive'
 
   export default {
     components: {
@@ -28,10 +36,29 @@ v-card
       }
     },
 
-    computed: mapGetters([]),
+    computed: {
+      ...mapGetters(["workingDirectoryId"]),
+
+      anyFiles() { return this.files.length > 0 }
+    },
 
     methods: {
       ...mapActions([ ]),
+
+      performUpload() {
+        const lastFile = this.files[this.files.length-1]
+
+        let reader = new FileReader()
+        reader.readAsDataURL(lastFile.file)
+        reader.onloadend = () => {
+          let fileContents = reader.result
+          fileContents = fileContents.replace(`data:${lastFile.type};base64,`, "")
+
+          drive.createFile(lastFile.name, lastFile.type, this.workingDirectoryId, fileContents, { base64: true })
+          .then(console.log)
+        }
+
+      }
     }
   }
 </script>
