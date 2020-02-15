328b6d404e325540e533b75edb514f05cc5966ce
Loren Norman <lorennorman@gmail.com>
2020-02-15 11:45:07 -0500

    add options to asset icon components

--- lib/components/icons/SpreadsheetIcon.vue
+++ lib/components/icons/SpreadsheetIcon.vue
@@ -1,18 +1,26 @@
 <template lang="pug">
-  v-tooltip(v-if="spreadsheetId" top)
-    | {{ spreadsheetId }}
+  v-tooltip(v-if="(spreadsheetId || force)" top)
+    | {{ spreadsheetId || "No Spreadsheet Id" }}
 
-    a(slot="activator" :href="driveLink" target="_blank")
-      v-icon mdi-google-spreadsheet
+    a(v-if="driveLink" slot="activator" :href="driveLink" target="_blank")
+      v-icon(:large="large") mdi-google-spreadsheet
+
+    v-icon(v-else slot="activator" :large="large") mdi-google-spreadsheet
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
-    props: ["spreadsheetId"],
+    props: ["spreadsheetId", "large", "force"],
 
     computed: {
+      ...mapGetters(["findSpreadsheet"]),
+
       driveLink() {
-        return `https://docs.google.com/spreadsheets/d/${this.spreadsheetId}/edit`
+        if(this.spreadsheetId && this.findSpreadsheet(this.spreadsheetId, false)) {
+          return `https://docs.google.com/spreadsheets/d/${this.spreadsheetId}/edit`
+        }
       }
     }
   }
