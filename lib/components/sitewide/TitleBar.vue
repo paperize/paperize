127be974d26fa6ea1a83ad393d97d2dfac3412cf
Loren Norman <lorennorman@gmail.com>
2020-06-28 10:12:13 -0400

    bump version for deploy

--- lib/components/sitewide/TitleBar.vue
+++ lib/components/sitewide/TitleBar.vue
@@ -4,7 +4,7 @@ v-toolbar(app)
     router-link(:to="{ name: homeLink }") Paperize.io
 
     v-tooltip
-      span.caption(slot="activator")= " ver.A7.2.0"
+      span.caption(slot="activator")= " ver.A7.4.0"
       | Alpha 7 "Obedient Consumer " {{ gitSha }}
 
   v-spacer
