f507c8d7c07c494cd8ab8b704d2469cba0808f96
Loren Norman <lorennorman@gmail.com>
2022-02-25 23:39:31 -0500

    forgot to bump version

--- lib/components/sitewide/TitleBar.vue
+++ lib/components/sitewide/TitleBar.vue
@@ -4,7 +4,7 @@ v-toolbar(app)
     router-link(:to="{ name: homeLink }") Paperize.io
 
     v-tooltip(bottom)
-      span.caption(slot="activator")= " ver.A8.1.3"
+      span.caption(slot="activator")= " ver.A8.1.4"
       | Alpha 8 "Personal Space" {{ gitSha }}
 
   v-spacer
