619f639b1b6f109c733fcac6e1d69480990be27f
Loren Norman <lorennorman@gmail.com>
2022-05-27 22:51:01 -0400

    install/config vite, purge webpack and babel

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { createVuePlugin } from 'vite-plugin-vue2'
+
+export default {
+  server: { port: 8080 }, // whatever you set in your google dev console
+  preview: {
+    port: 8080,
+    open: true
+  },
+  plugins: [
+    createVuePlugin()
+  ],
+}
