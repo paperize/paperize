7ef122d12b27ba15a31831aa05964937beeceacf
Loren Norman <lorennorman@gmail.com>
2023-01-09 09:40:30 -0500

    bump version

--- lib/components/sitewide/TitleBar.vue
+++ lib/components/sitewide/TitleBar.vue
@@ -5,7 +5,7 @@ v-app-bar
 
     v-tooltip(bottom)
       template(v-slot:activator="{ on }")
-        span.caption(v-on="on")= " ver.A8.7.2"
+        span.caption(v-on="on")= " ver.A8.8.0"
       | Alpha 8 "Personal Space" {{ gitSha }}
 
   v-spacer
